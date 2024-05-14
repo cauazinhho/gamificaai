0b96bee6cf186022cbea64c2afe4a3b4fedca036
cauazinhho <emerson.cauarodrigues@gmail.com>
2024-05-14 17:22:59 -0300

    feat: carrosel feito

--- script.js
+++ script.js
@@ -4,27 +4,27 @@ let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.querySelector("#icone-x")
 
-function abrirFecharMenu(){
+function abrirFecharMenu() {
     // Se o menu está fechado
 
-    if (menu.classList.contains("menu-fechado")){
+    if (menu.classList.contains("menu-fechado")) {
         // Abrir o menu
         menu.classList.remove("menu-fechado")
 
         // Mostrar icone X
         iconeX.style.display = "inline"
-        
+
         // Esconder icone Barras
         iconeBarras.style.display = "none"
 
     } else {
         menu.classList.add("menu-fechado")
 
-          // Esconder icone X
-          iconeX.style.display = "none"
-        
-          // Mostrar icone Barras
-          iconeBarras.style.display = "inline"
+        // Esconder icone X
+        iconeX.style.display = "none"
+
+        // Mostrar icone Barras
+        iconeBarras.style.display = "inline"
     }
 }
 
@@ -32,4 +32,54 @@ window.onresize = () => {
     menu.classList.remove("menu-fechado")
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
+}
+
+//Funcao Carrosel
+
+let slides = [
+    'primeiro-banner',
+    'segundo-banner',
+    'terceiro-banner'
+]
+
+let slideAtual = 0
+
+let numeroSlides = slides.length
+
+let banner = document.querySelector(".banner")
+
+banner.classList.add(slides[slideAtual])
+
+
+//fazendo uma função aos botões
+
+const mostrarProximoSlide = () => {
+    //Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    //verificação para voltar para inicio
+    if (slideAtual < (numeroSlides - 1)) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
+}
+
+const mostrarAnteriorSlide = () => {
+    //Remove slide anterior
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0) {
+        slideAtual--
+    } else {
+        slideAtual = numeroSlides - 1
+    }
+
+    slideAtual--
+
+    // Renderiza o slideAtual
+    banner.classList.add(slides[slideAtual])
 }
